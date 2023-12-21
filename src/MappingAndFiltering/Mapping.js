9ee189c04a09e19376d9f08bbb498ce5deeaf5b0
MVGS <vigneshbalanmvgs2003@gmail.com>
2023-12-21 21:18:58 +0530

    Mapped Array

--- src/MappingAndFiltering/Mapping.js
+++ src/MappingAndFiltering/Mapping.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+
 
 const Mapping = () => {
+
+        const [ users, setUsers] = useState([]);
+        useEffect(() => {
+            fetch("https://jsonplaceholder.typicode.com/users")
+                .then((response) => response.json())
+                .then((json) => console.log(json));
+        }, []);
+    
+    const mapData = () => {
+        let mappedArray = users.map((user) => user.id * 2)
+        console.log(mappedArray);
+    }
+    
   return (
-      <div>
-      <div>Mapping</div>
-      <div>Mapping</div>
-      <div>Mapping</div>
-      <div>Mapping</div>
-      <div>Mapping</div>
-      <div>Mapping</div>
-      <div>Mapping</div>
-      <div>Mapping</div>
+      <div className='MappingF'>
+          <h1>Users</h1>
+          <div>
+              <div>
+                  {users.map((user) => (
+                      
+                      <div>
+                          <p>{user.name }</p>
+                          <p>{user.username}</p>
+                      </div>
+                  ))}
+                  <button onClick={mapData}>see mapped arrya</button>
+                  <p>name</p>
+                  <p>something</p>
+              </div>
+          </div>
+        
               
       </div>
       )
